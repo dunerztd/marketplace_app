564ceb41878e77fa189d376cd9b35ce32a8f3708
Tom D <dunerz@hotmail.com>
2020-08-01 21:55:34 +1000

    split up navbar into guest/student/teacher partials

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,13 @@ class User < ApplicationRecord
   has_one :teacher, dependent: :destroy
   has_many :bookings
 
-  # Validates Full Name for user signup
+  # Validates Full Name for user sign-up
   validates :fullname, presence: true, length: { in: 1...150 }
 
-  # Validates email for User signup
+  # Validates email for User sign-up
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { in: 1...75 }
 
   # Validates password for user sign-up
   validates :password, confirmation: true, presence: true, length: { in: 1...50 }
-
-  # Validates password confirmation for user sign-up
-  # validates :password_confirmation
 
 end
